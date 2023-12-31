68548bbd8537cf22712af2e9d0a4fddeb6e68846
Prince Soni <132805751+NyXdeity@users.noreply.github.com>
2024-01-01 00:05:03 +0530

    udate

--- home.js
+++ home.js
@@ -1,50 +1,50 @@
-// const apiKey = "3945d5ffeed3814f174dc627b1d3bdcc";
-// // const apiKey = "edcccd400d30778e5ad6e9b4aee8f925";
-// const apiUrl = "https://api.openweathermap.org/data/2.5/weather?appid=3945d5ffeed3814f174dc627b1d3bdcc&q=";
-// const weatherIcon = document.querySelector(".weather-icon");
+const apiKey = "3945d5ffeed3814f174dc627b1d3bdcc";
+// const apiKey = "edcccd400d30778e5ad6e9b4aee8f925";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?appid=3945d5ffeed3814f174dc627b1d3bdcc&q=";
+const weatherIcon = document.querySelector(".weather-icon");
 
 
-// const searchBox = document.querySelector(".search input");
-// const searchBtn = document.querySelector(".search button");
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
 
 
-// async function checkWeather(city){
+async function checkWeather(city){
 
 
-//     const response = await fetch(apiUrl + city + "&units=metric");
+    const response = await fetch(apiUrl + city + "&units=metric");
 
-//     if(response.status == 404){
-//         document.querySelector(".error").style.display = "block";
-//         document.querySelector(".weather").style.display = "none";
-//     }
-//     else{
-//         var data = await response.json();
+    if(response.status == 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }
+    else{
+        var data = await response.json();
 
-//         document.querySelector(".city").innerHTML = data.name;
-//         document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c" ; 
-//         document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
-//         document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c" ; 
+        document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
         
-//         if(data.weather[0].main == "Clouds"){
-//             weatherIcon.src = "weather-app-img/images/clouds.png"
-//         }
-//         else if(data.weather[0].main == "Clear"){
-//             weatherIcon.src = "weather-app-img/images/clear.png"
-//         } 
-//         else if(data.weather[0].main == "Rain"){
-//             weatherIcon.src = "weather-app-img/images/rain.png"
-//         } 
-//         else if(data.weather[0].main == "Drizzle"){
-//             weatherIcon.src = "weather-app-img/images/drizzle.png"
-//         } 
-//         else if(data.weather[0].main == "Mist"){
-//             weatherIcon.src = "weather-app-img/images/mist.png"
-//         } 
+        if(data.weather[0].main == "Clouds"){
+            weatherIcon.src = "weather-app-img/images/clouds.png"
+        }
+        else if(data.weather[0].main == "Clear"){
+            weatherIcon.src = "weather-app-img/images/clear.png"
+        } 
+        else if(data.weather[0].main == "Rain"){
+            weatherIcon.src = "weather-app-img/images/rain.png"
+        } 
+        else if(data.weather[0].main == "Drizzle"){
+            weatherIcon.src = "weather-app-img/images/drizzle.png"
+        } 
+        else if(data.weather[0].main == "Mist"){
+            weatherIcon.src = "weather-app-img/images/mist.png"
+        } 
 
-//         document.querySelector(".weather").style.display = "block";
-//         document.querySelector(".error").style.display = "none";
-//     }
-// }
-// searchBtn.addEventListener("click", () => {
-//     checkWeather(searchBox.value);
-// })
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+    }
+}
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+})
